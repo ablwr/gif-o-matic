80c0b45224496b59856c6240af9e0781fbc26a80
Kieran O'Leary <kieran.o.leary@gmail.com>
2017-03-11 09:43:50 +0000

    main.js - only allows file selection

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const dialog = require('electron').dialog
 
 ipc.on('open-file-dialog', function (event) {
   dialog.showOpenDialog({
-    properties: ['openFile', 'openDirectory']
+    properties: ['openFile']
   }, function (files) {
     if (files) event.sender.send('selected-directory', files)
   })
